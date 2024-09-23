afc2df561bab7f6a168892a291636036c1f55422
Isabella <142615084+Isacrff@users.noreply.github.com>
2024-09-23 10:14:13 -0300

    a

--- main.js
+++ main.js
@@ -12,6 +12,6 @@ function areaQuadrado(lado){
 console.log(areaQuadrado(5));
 
 let areaTriangulo=function(base, altura){
-    return base*2;
+    return base*altura/2;
 }
 console.log(areaTriangulo(5))
