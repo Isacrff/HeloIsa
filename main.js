3de08e86e2d13c190b2ebcc25e2ccf72b35109a5
Isabella <142615084+Isacrff@users.noreply.github.com>
2024-09-30 10:28:43 -0300

    a

--- main.js
+++ main.js
@@ -36,7 +36,7 @@
 let estados = ['Paraná', 'São paulo', 'Minas Gerais', 'Rio de Janeiro', 'Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Pará','Paraíba','Pernambuco','Piauí','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','Sergipe','Tocantins','Distrito Federal']
 
 let i=0;
-while (i<estados.length){}
+while (i<estados.length){
     console.log(i);
     console.log(estados[i]);
    i++;
